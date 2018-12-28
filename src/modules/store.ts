192c41f1fa0c6dc7b69dc9406ea3674af611396b
Darius Jankauskas <d5926j@gmail.com>
2018-12-28 17:45:30 -0500

    added Redux DevTools support

--- src/modules/store.ts
+++ src/modules/store.ts
@@ -2,4 +2,4 @@ import { createStore } from 'redux';
 import { reducer } from './reducer';
 import { initialState } from './state';
 
-export let store = createStore(reducer, initialState);
+export let store = createStore(reducer, initialState, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__());
