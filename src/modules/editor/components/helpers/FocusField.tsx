c264f04ef87772b95045ff7a671b848beb14f55c
Darius Jankauskas <d5926j@gmail.com>
2019-01-04 15:07:53 -0500

    refactored to use rmwc text fields

--- src/modules/editor/components/helpers/FocusField.tsx
+++ src/modules/editor/components/helpers/FocusField.tsx
@@ -1,38 +1,29 @@
 import * as React from 'react';
 import './EditorHelpers.css'
 
+import { TextField } from '@rmwc/textfield';
+
 interface IProps {
     value: string,
     onChange: (s: string) => void,
-    isLabelVisible?: boolean,
-    touched?: boolean,
     error?: string,
     label: string,
-
 }
 
 export const FocusField: React.SFC<IProps> = ({
     value,
     onChange,
     label,
-    isLabelVisible = true,
-    touched,
     error
 }) => {
     return (
-        <div className="EditorFieldDiv">
-            {isLabelVisible && <label>{label}</label>}
-            <div>
-                <span
-                    className="FocusFieldInput"
-                    onInput={(e: React.FormEvent<HTMLSpanElement>) => { onChange(e.currentTarget.innerText) }}
-                    contentEditable={true}
-                    placeholder={label}
-                    defaultValue={value}
-                />
-                {touched &&
-                    (error && <p className="FocusFieldValidation">{error}</p>)}
-            </div>
-        </div>
+        <TextField
+            className="EditorField"
+            value={value}
+            onChange={(e: React.FormEvent<HTMLInputElement>) => onChange(e.currentTarget.value)}
+            label={label}
+            outlined={true}
+            size={75}
+        />
     );
 };
--- src/modules/editor/components/helpers/NumberField.tsx
+++ src/modules/editor/components/helpers/NumberField.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import './EditorHelpers.css';
 
+import { TextField } from '@rmwc/textfield';
+
 interface IProps {
     value: string,
     onChange: (s: string) => void,
-    isLabelVisible?: boolean,
-    touched?: boolean,
     error?: string,
     label: string,
     autoComplete?: string,
@@ -15,28 +15,20 @@ export const NumberField: React.SFC<IProps> = ({
     value,
     onChange,
     label,
-    isLabelVisible = true,
-    touched,
     error,
     autoComplete
 }) => {
-    const change = (e: React.FormEvent<HTMLInputElement>) => { onChange(e.currentTarget.value) };
     return (
-        <div className="EditorFieldDiv">
-            {isLabelVisible && <label>{label}</label>}
-            <div>
-                <input
-                    className="NumberFieldInput"
-                    value={value}
-                    onChange={change}
-                    autoComplete={autoComplete}
-                    placeholder={label}
-                    type="text"
-                />
-                {touched &&
-                    (error && <p className="EditorFieldValidation">{error}</p>)}
-            </div>
-        </div>
+        <TextField
+            className="EditorField"
+            value={value}
+            onChange={(e: React.FormEvent<HTMLInputElement>) => onChange(e.currentTarget.value)}
+            outlined={true}
+            size={5}
+            label={label}
+            autoComplete={autoComplete}
+            pattern="\d*"
+        />
     )
 };
 
