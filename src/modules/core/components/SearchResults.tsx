a70ee016aed2886055b59b02277509aa48ce5201
Darius Jankauskas <d5926j@gmail.com>
2019-02-12 07:27:44 -0500

    Updated props passed to ArticleCard

--- src/modules/core/components/SearchResults.tsx
+++ src/modules/core/components/SearchResults.tsx
@@ -1,23 +1,34 @@
 import * as React from 'react';
 
 import { Typography } from '@rmwc/typography';
-import { ArticleCard } from './ArticleCard';
+import { ArticleCard, } from './ArticleCard';
 
 const cardPadding = {
     marginBottom: "10px"
 }
 
-interface IProps {
-    results: Array<{ id: string } | undefined>
+interface IResults {
+    searchable: {
+        id: string,
+        title: string,
+        preview?: string,
+        slug: string,
+        contributors?: Array<{ first_name?: string, last_name?: string, slug: string }>,
+        section: { permalink: string }
+    }
 }
 
-export const SearchResults: React.SFC<IProps> = ({results}) => (
+interface IProps {
+    results: Array<IResults | undefined>
+}
+
+export const SearchResults: React.SFC<IProps> = ({ results }) => (
     <>
         <Typography use="headline3"><b>Search Results</b></Typography>
         {
             results.map(a => a ?
                 <div style={cardPadding}>
-                    <ArticleCard id={a.id} />
+                    <ArticleCard data={a.searchable} />
                 </div>
                 : undefined
             )
