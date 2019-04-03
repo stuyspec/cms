1d3813977f949729699fa06e0448e06b2fed229b
Darius Jankauskas <d5926j@gmail.com>
2019-04-02 21:28:15 -0400

    Now redirects to sign-in on invalid session.

--- src/modules/accounts/components/AuthorizedRoute.tsx
+++ src/modules/accounts/components/AuthorizedRoute.tsx
@@ -1,33 +1,53 @@
 import * as React from 'react';
 
+import { Route, Redirect, RouteComponentProps } from 'react-router';
+
 import { connect } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import { validateToken } from '../actions';
 
 import { PermissionLevel } from '../permissionLevel';
 import { IState } from '../../state';
+import { IAccountsSession } from '../state';
 
 interface IProps {
-    component: React.ComponentType<any>,
+    component?: React.ComponentType<any>,
+    render?: ((props: RouteComponentProps<any>) => React.ReactNode),
     auth: PermissionLevel,
     path: string,
     key: string,
-    session: boolean
+    exact?: boolean,
+    session: IAccountsSession | null,
+    dispatch: any
 }
 
-const AuthorizedRouteUnconnected: React.SFC<IProps> = (props: IProps) => {
-    if(props.session) {
-        return <Route component={props.component} path={props.path} key={props.key} />;
+class AuthorizedRouteUnconnected extends React.Component<IProps, {}> {
+    constructor(props: IProps) {
+        super(props);
+        if(props.session) {
+            validateToken(props.dispatch, props.session)
+        }
     }
-    return <Redirect to="/sign-in" />;
+
+    render() {
+        if (this.props.session) {
+            return <Route
+                component={this.props.component}
+                render={this.props.render}
+                path={this.props.path}
+                key={this.props.key}
+                exact={this.props.exact}
+            />;
+        }
+        return <Redirect to="/sign-in" />;
+    }
 }
 
 const mapStateToProps = (state: IState) => {
     return {
-        session: !!state.accounts.session
+        session: state.accounts.session
     }
 }
 
 export const AuthorizedRoute = connect(
-    mapStateToProps,
-    null
+    mapStateToProps
 )(AuthorizedRouteUnconnected);
