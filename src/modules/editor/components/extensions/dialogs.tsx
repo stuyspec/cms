2482d8cce69e1c62626fa37e7fc92c2a9f0fe516
Darius Jankauskas <d5926j@gmail.com>
2019-12-08 14:46:17 -0500

    Added MediaExtensionDialog

--- src/modules/editor/components/extensions/dialogs.tsx
+++ src/modules/editor/components/extensions/dialogs.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 
-export interface IExtensionDialogProps {
-    props?: any,
-    onSubmit: (props: any | null) => any,
-    open: boolean
-}
-
 import { LineChartDialog } from './LineChartDialog';
 import { BarChartDialog } from './BarChartDialog';
 import { PieChartDialog } from './PieChartDialog'
+import { MediaExtensionDialog } from './MediaExtensionDialog';
+import { IMedium } from '../../queryHelpers';
+
+interface ISubmit {
+    props?: any,
+    media?: number[]
+}
+
+export interface IExtensionDialogProps {
+    props?: any,
+
+    //ids of media objects currently attached to 
+    mediaIds?: number[],
+    //array of all media objects currently available to article
+    allMedia: IMedium[],
+    onMediumAdd: (m: IMedium) => any
+    
+    onSubmit: (data: ISubmit | null) => any,
+    open: boolean
+}
 
 export const dialogs = new Map<string, React.ComponentType<IExtensionDialogProps>>([
     ["LineChartExtension", LineChartDialog],
     ["BarChartExtension", BarChartDialog],
-    ["PieChartExtension", PieChartDialog]
+    ["PieChartExtension", PieChartDialog],
+    ["MediaExtension", MediaExtensionDialog]
 ]);
