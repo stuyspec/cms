0668df0732ac3e1ca78af1b4fec78a4410602e8c
Darius Jankauskas <d5926j@gmail.com>
2019-12-08 14:41:16 -0500

    Fix reducer typing

--- src/modules/reducer.ts
+++ src/modules/reducer.ts
@@ -3,7 +3,7 @@ import { IState, initialState } from './state';
 import { IGenericAction } from "./betterRedux";
 
 export function reducer(state: IState | undefined, action: any): IState {
-    if((action as IGenericAction<IState>).handler) {
+    if((action as IGenericAction<IState> | undefined)?.handler) {
         return action.handler(state)
     }
     return state || initialState;
