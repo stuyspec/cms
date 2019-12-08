1f586d7cf36a1b41472bd0deffd18b11d2815d8b
Darius Jankauskas <d5926j@gmail.com>
2019-12-08 14:36:10 -0500

    Add global snackbarQueue

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import { client } from './modules/apolloClient';
 import { RoutingApp } from './modules/RoutingApp';
 import { store } from './modules/store';
 
+//allow simple display of snackbars throughout app
+import { SnackbarQueue } from '@rmwc/snackbar';
+import { snackbarQueue } from './modules/snackbarQueue';
+
 
 class App extends React.Component {
   public render() {
@@ -17,6 +21,7 @@ class App extends React.Component {
         <Provider store={store}>
           <ApolloProvider client={client}>
             <RoutingApp />
+            <SnackbarQueue messages={snackbarQueue.messages} />
           </ApolloProvider>
         </Provider>
       </div>
