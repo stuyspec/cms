e905e1ff845fc95317f0f80a3dd4abe3dc1803fa
David Chen <76599476+dchen278@users.noreply.github.com>
2020-12-26 12:46:13 -0500

    Update CreateArticleForm.tsx

--- src/modules/editor/components/CreateArticleForm.tsx
+++ src/modules/editor/components/CreateArticleForm.tsx
@@ -102,6 +102,13 @@ export const CreateArticleUnconnected: React.FC<any> = (props) => {
         return <Redirect to={redirectTo} />
     }
 
+    function redirectToArticle() {
+        var title = document.getElementsByClassName("mdc-text-field__input")[0] as HTMLInputElement
+        var titleString = title.value.toLowerCase()
+        titleString = titleString.replace(/[^A-Z0-9]+/ig, "-")
+        return `/draft/edit/${titleString}`
+    }
+
     return (
         <>
             <CreateArticleMutation
@@ -115,7 +122,7 @@ export const CreateArticleUnconnected: React.FC<any> = (props) => {
                         title: `Successfully created ${props.publish ? 'article' : 'draft'}.`, 
                         timeout: 2000
                     });
-                    setRedirectTo(props.publish ? '/articles' : '/')
+                    setRedirectTo(props.publish ? '/articles' : redirectToArticle())
                 }}
             >
                 {(mutate) => (
